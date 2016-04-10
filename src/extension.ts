05b8e46ba7d3832d5a323ecacd1c48f8ab7bcf07
Alan Dao <Alan Dao>
2016-04-09 20:00:29 -0700

    added command to launch Love2D

--- src/extension.ts
+++ src/extension.ts
@@ -2,26 +2,22 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {spawn} from 'child_process';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "love2d-ide" is now active!');
-
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    let disposable = vscode.commands.registerCommand('extension.runLove', () => {
+        let config = vscode.workspace.getConfiguration('love2d');
+        let love2dPath = config.get("path") as string;
+        var child = spawn(love2dPath, [])
     });
-
     context.subscriptions.push(disposable);
+
 }
 
 // this method is called when your extension is deactivated
